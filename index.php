d94ad5e99596c735104d3ce47fcbeb694c28fc94
shubkb07 <shub@shubkb.com>
2025-03-10 11:40:04 +0530

    Add main entry point and coding standards configuration for Pay application

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This is the main entry point for the Pay application.
+ * 
+ * PHP version 8.0.0
+ * 
+ * @category pay
+ * @package  Pay
+ * @author   Shubham Kumar Bansal <shub@shubkb.com>
+ * @license  MIT License
+ * @version  1.0.0
+ * @link     https://pay.sh6.me/
+ */
+
+! defined('ABSPATH') && exit;
+echo 'test';
+echo '<br>';
+echo ABSPATH;
