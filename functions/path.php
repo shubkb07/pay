3d1a4565d7c3a48b48172449b6c6b5f592e94539
shubkb07 <shub@shubkb.com>
2025-03-10 16:58:22 +0530

    Add debugging output for home path in path.php

--- functions/path.php
+++ functions/path.php
@@ -14,6 +14,7 @@ if (count($path) === 1) {
     header('Content-Type: text/html; charset=utf-8');
     var_dump($path);
     if ($path[0] === 'home' || $path[0] === '') {
+        var_dump('home');
         include_once ASSETS . 'html/home.php';
     } elseif ($path[0] === 'about') {
         include_once ASSETS . 'html/about.php';
