0785d064cd85e0f0361864278ff23e4cb6808636
shubkb07 <shub@shubkb.com>
2025-03-12 02:08:32 +0530

    Add routing for payment success, failure, and processing pages; create 404 and pay page templates

--- functions/path.php
+++ functions/path.php
@@ -16,6 +16,21 @@ if (count($path) > 0) {
             if ($path[2] === 'webhook' && $path[3] === 'pay') {
             }
         }
+    } elseif ($path[0] === 'pay') {
+        if (count($path) === 3) {
+            $pay_id = $path[2];
+            if ($path[1] === 'success') {
+                include_once ASSETS . 'pages/success.php';
+            } elseif ($path[1] === 'failed') {
+                include_once ASSETS . 'pages/failed.php';
+            } elseif ($path[1] === 'p') {
+                include_once ASSETS . 'pages/pay.php';
+            } elseif ($path[1] === 'o') {
+                $pay->redirect_pay($pay_id);
+            } else {
+                include_once ASSETS . 'pages/404.php';
+            }
+        }
     } elseif (count($path) === 1) {
         header('Content-Type: text/html; charset=utf-8');
         if ($path[0] === 'home' || $path[0] === '') {
