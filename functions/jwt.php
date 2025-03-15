e8be4ccf09e477f933d94e11edc28b0a7d7700e3
shubkb07 <shub@shubkb.com>
2025-03-15 23:46:40 +0530

    Uncomment JWT token creation function for active use

--- functions/jwt.php
+++ functions/jwt.php
@@ -11,11 +11,11 @@ use Firebase\JWT\{JWT, key};
 /**
  * JWT Create Token.
  */
-// function jwt_create_token($data) {
-//     include_once INC . 'lib/firebase/php-jwt/src/JWT.php';
-//     $key = 'secret';
-//     return JWT::encode($data, $key, 'HS256');
-// }
+function jwt_create_token($data) {
+    include_once INC . 'lib/firebase/php-jwt/src/JWT.php';
+    $key = 'secret';
+    return JWT::encode($data, $key, 'HS256');
+}
 
 // /**
 //  * JWT Decode Token.
@@ -29,10 +29,10 @@ use Firebase\JWT\{JWT, key};
 //     return [$header, $decoded];
 // }
 
-// $text = 'Hello World!';
+$text = 'Hello World!';
 // $token = jwt_create_token($text);
-// echo '<br>';
+echo '<br>';
 // var_dump($token);
-// echo '<br>';
+echo '<br>';
 // var_dump(jwt_decode_token($token));
-// echo '<br>';
+echo '<br>';
