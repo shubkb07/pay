5f9191352a38c096265a07679e2e70b82ce9fd29
shubkb07 <shub@shubkb.com>
2025-03-16 00:05:15 +0530

    Simplify jwt_decode_token function by combining decoding and conversion to associative array

--- functions/jwt.php
+++ functions/jwt.php
@@ -24,21 +24,5 @@ function jwt_decode_token($token) {
     include_once INC . 'lib/php-jwt/src/JWT.php';
     include_once INC . 'lib/php-jwt/src/Key.php';
     $key = 'secret';
-    $decoded = JWT::decode($token, new Key($key, 'HS256'));
-    // Convert the object into an associative array.
-    $decoded = json_decode(json_encode($decoded), true);
-    return $decoded;
+    return json_decode(json_encode(JWT::decode($token, new Key($key, 'HS256'))), true);
 }
-
-echo '<br>';
-echo 'random 4';
-echo '<br>';
-$text = array('name' => 'Some Name', 'email' => 'example@example.com');
-$token = jwt_create_token($text);
-echo '<br>';
-var_dump($token);
-echo '<br>';
-echo '<pre>';
-print_r(jwt_decode_token($token));
-echo '</pre>';
-echo '<br>';
