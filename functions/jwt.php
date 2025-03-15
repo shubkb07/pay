f1ff3f5f7fcc458926f98c12d21ea9fc47f9e0f3
shubkb07 <shub@shubkb.com>
2025-03-15 23:45:47 +0530

    Comment out JWT token handling functions for future reference

--- functions/jwt.php
+++ functions/jwt.php
@@ -11,38 +11,28 @@ use Firebase\JWT\{JWT, key};
 /**
  * JWT Create Token.
  */
-function jwt_create_token($data) {
-    include_once INC . 'lib/firebase/php-jwt/src/JWT.php';
-    $key = 'secret';
-    return JWT::encode($data, $key, 'HS256');
-}
+// function jwt_create_token($data) {
+//     include_once INC . 'lib/firebase/php-jwt/src/JWT.php';
+//     $key = 'secret';
+//     return JWT::encode($data, $key, 'HS256');
+// }
 
-/**
- * JWT Decode Token.
- */
-function jwt_decode_token($token) {
-    include_once INC . 'lib/firebase/php-jwt/src/JWT.php';
-    include_once INC . 'lib/firebase/php-jwt/src/Key.php';
-    $key = 'secret';
-    $header = new StdClass();
-    $decoded = JWT::decode($jwt, new Key($key, 'HS256'), $headers);
-    return [$header, $decoded];
-}
+// /**
+//  * JWT Decode Token.
+//  */
+// function jwt_decode_token($token) {
+//     include_once INC . 'lib/firebase/php-jwt/src/JWT.php';
+//     include_once INC . 'lib/firebase/php-jwt/src/Key.php';
+//     $key = 'secret';
+//     $header = new StdClass();
+//     $decoded = JWT::decode($jwt, new Key($key, 'HS256'), $headers);
+//     return [$header, $decoded];
+// }
 
-$text = 'Hello World!';
-$token = jwt_create_token($text);
-echo '<br>';
-var_dump($token);
-echo '<br>';
-var_dump(jwt_decode_token($token));
-echo '<br>';
-
-
-/**
- * JWT Verify Token.
- */
-function jwt_verify_token($token) {
-    $key = 'secret';
-    return JWT::decode($token, new Key($key, 'HS256'));
-}
-
+// $text = 'Hello World!';
+// $token = jwt_create_token($text);
+// echo '<br>';
+// var_dump($token);
+// echo '<br>';
+// var_dump(jwt_decode_token($token));
+// echo '<br>';
